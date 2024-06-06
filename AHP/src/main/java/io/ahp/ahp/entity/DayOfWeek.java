31b0a875dd51132fe15eb5ebde1b6839c1fcad58
Nduwarugir <nduwarugirabruno@gmail.com>
2024-06-06 17:11:05 +0100

    commit du 06/06/2023 avec : 1- restructuration des classes, 2- debut de l'AHP.

--- AHP/src/main/java/io/ahp/ahp/entity/DayOfWeek.java
+++ AHP/src/main/java/io/ahp/ahp/entity/DayOfWeek.java
@@ -1,13 +1,6 @@
 package io.ahp.ahp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 // Entité TimeSlot
 @Table(name = "day_of_week")
